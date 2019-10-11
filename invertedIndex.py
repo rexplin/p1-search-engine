d7740f85301638424cd90e4a4ead09bcb53e5579
Garrett Allen <neelik.n33l@gmail.com>
2019-10-11 01:29:15 -0600

    Reformulated the writing of the tree to remove all race conditions introduced by multi-thread writing.

--- invertedIndex.py
+++ invertedIndex.py
@@ -10,6 +10,56 @@ import threading
 import sys
 
 
+def parsing_worker(queue, parsed_docs_queue):
+    while True:
+        item = queue.get()
+
+        if item is None or queue.empty():
+            break
+
+        parse_document(item, parsed_docs_queue)
+        queue.task_done()
+
+    # Make sure last task completes or it locks
+    queue.task_done()
+
+
+def parse_document(document, parsed_docs_queue):
+
+    # Pre-process the content of the document
+    unique_id = 1
+    doc_id = document["id"]
+    stemmer = nltk.stem.snowball.EnglishStemmer()
+    tokens = nltk.word_tokenize(f"{document['title']} {document['content']}")
+    ascii_tokens = [ascii_token for ascii_token in tokens if is_ascii(ascii_token)]
+    filtered = [token.lower() for token in ascii_tokens if token not in stop_words]
+    processed_tokens = [stemmer.stem(token) for token in filtered if token.isalpha()]
+
+    # Process one more time for stop words after lemmatizing cause we might still have things like 'a' leftover
+    final_tokens = [token for token in processed_tokens if token not in stop_words]
+    # pp_docs_queue.put({"id": doc_id, "text": " ".join(final_tokens)})
+
+    for token in final_tokens:
+        pos = unique_id
+        parsed_docs_queue.put({"token": token, "doc_id": doc_id, "position": pos})
+
+        unique_id += 1
+
+
+def is_ascii(text):
+    if isinstance(text, str):
+        try:
+            text.encode('ascii')
+        except UnicodeEncodeError:
+            return False
+    else:
+        try:
+            text.decode('ascii')
+        except UnicodeDecodeError:
+            return False
+    return True
+
+
 def sort_coo(coo_matrix):
     tuples = zip(coo_matrix.col, coo_matrix.data)
     return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
@@ -39,93 +89,12 @@ def sort_coo(coo_matrix):
 #     return results
 
 
-def index_worker(queue, index_queue, pp_docs_queue):
-    while True:
-        item = queue.get()
-
-        if item is None or queue.empty():
-            break
-
-        do_work(item, index_queue, pp_docs_queue)
-        queue.task_done()
-
-    # Make sure last task completes or it locks
-    queue.task_done()
-
-
-def do_work(document, index_tree, pp_docs_queue):
-    # process
-    index = dict()
-    unique_id = 1
-    doc_id = document["id"]
-    stemmer = nltk.stem.snowball.EnglishStemmer()
-    tokens = nltk.word_tokenize(f"{document['title']} {document['content']}")
-    ascii_tokens = [ascii_token for ascii_token in tokens if is_ascii(ascii_token)]
-    filtered = [token.lower() for token in ascii_tokens if token not in stop_words]
-    processed_tokens = [stemmer.stem(token) for token in filtered if token.isalpha()]
-
-    # Process one more time for stop words after lemmatizing cause we might still have things like 'a' leftover
-    final_tokens = [token for token in processed_tokens if token not in stop_words]
-    pp_docs_queue.put({"id": doc_id, "text": " ".join(final_tokens)})
-
-    for token in final_tokens:
-        pos = unique_id
-        try:
-            existing = tree.get(token)
-            if existing:
-                existing = existing.decode()
-                updated = "|".join([existing, f"{doc_id}:{pos}"])
-                index_tree.insert(token, updated.encode(), replace=True)
-            else:
-                index_tree.insert(token, f"{doc_id}:{pos}".encode())
-        except AssertionError:
-            with open("error-log.txt", "a") as log:
-                log.write(f"Assertion: {token} - {doc_id}:{pos}")
-                log.write('\n')
-            continue
-        except ReachedEndOfFile:
-            with open("error-log.txt", "a") as log:
-                log.write(f"EOF: {token} - {doc_id}:{pos}")
-                log.write('\n')
-            continue
-        except IndexError:
-            with open("error-log.txt", "a") as log:
-                log.write(f"Index: {token} - {doc_id}:{pos}")
-                log.write('\n')
-            continue
-
-        unique_id += 1
-    # for token in final_tokens:
-    #     # add the document id followed by the line location into the dictionary/list
-    #     pos = unique_id
-    #     if token not in index:
-    #         index.update({token: [f"{doc_id}:{pos}"]})
-    #     else:
-    #         index[token].append(f"{doc_id}:{pos}")
-    #     unique_id += 1
-    #
-    # index_queue.put(index)
-
-
-def is_ascii(text):
-    if isinstance(text, str):
-        try:
-            text.encode('ascii')
-        except UnicodeEncodeError:
-            return False
-    else:
-        try:
-            text.decode('ascii')
-        except UnicodeDecodeError:
-            return False
-    return True
-
-
 if __name__ == "__main__":
     work = Queue()
+    parsed_docs = Queue()
     # results = Queue()
-    tree = BPlusTree("./index/index.db", serializer=StrSerializer(), order=50, key_size=20)
-    pp_docs = Queue()
+    tree = BPlusTree("./index/full-index.db", serializer=StrSerializer(), order=50, key_size=20)
+    # pp_docs = Queue()
     num_workers = 8
     threads = []
     total_index = dict()
@@ -133,23 +102,56 @@ if __name__ == "__main__":
 
     try:
         # produce data
-        with open("test_data_lines.json", "r") as f:
-        # with open("wikipedia_data_lines.json", "r") as f:
+        # with open("test_data_lines.json", "r") as f:
+        with open("wikipedia_data_lines.json", "r") as f:
             for entry in f:
-                if count == 10000:
-                    break
+                # if count == 10000:
+                #     break
                 work.put(simplejson.loads(entry))
                 count += 1
 
         # start for workers
-        for i in range(num_workers):
-            t = threading.Thread(target=index_worker, args=(work, tree, pp_docs))
+        for i in range(num_workers - 1):
+            t = threading.Thread(target=parsing_worker, args=(work, parsed_docs))
             t.daemon = True
             t.start()
             threads.append(t)
 
         print("Performing work.join()")
         work.join()
+
+        print("Writing the tree...")
+        while not parsed_docs.empty():
+            parsed_doc = parsed_docs.get()
+            if parsed_doc is not None:
+                try:
+                    existing = tree.get(parsed_doc["token"])
+                    if existing:
+                        existing = existing.decode()
+                        updated = "|".join([existing, f"{parsed_doc['doc_id']}:{parsed_doc['position']}"])
+                        tree.insert(parsed_doc["token"], updated.encode(), replace=True)
+                        parsed_docs.task_done()
+                    else:
+                        tree.insert(parsed_doc["token"], f"{parsed_doc['doc_id']}:{parsed_doc['position']}".encode())
+                        parsed_docs.task_done()
+                except AssertionError:
+                    with open("error-log.txt", "a") as log:
+                        log.write(f"Assertion: {parsed_doc['token']} - {parsed_doc['doc_id']}:{parsed_doc['position']}")
+                        log.write('\n')
+                    parsed_docs.task_done()
+                    continue
+                except ReachedEndOfFile:
+                    with open("error-log.txt", "a") as log:
+                        log.write(f"EOF: {parsed_doc['token']} - {parsed_doc['doc_id']}:{parsed_doc['position']}")
+                        log.write('\n')
+                    parsed_docs.task_done()
+                    continue
+                except IndexError:
+                    with open("error-log.txt", "a") as log:
+                        log.write(f"Index: {parsed_doc['token']} - {parsed_doc['doc_id']}:{parsed_doc['position']}")
+                        log.write('\n')
+                    parsed_docs.task_done()
+                    continue
 
         # get the results
         # print("Retrieving results and building final index")
@@ -190,22 +192,16 @@ if __name__ == "__main__":
 
         # Clean up
         print("Terminating workers...")
-        for i in range(num_workers):
+        for i in range(num_workers - 1):
             work.put(None)
+
+        parsed_docs.put(None)
 
         print("Terminating threads...")
         for t in threads:
             t.join()
 
-        # print("Writing index file...")
-        # ordered_index = OrderedDict(sorted(total_index.items()))
-        # with open("ascii_index.json", "w") as file:
-        #     file.write(simplejson.dumps(ordered_index))
-        #     file.write("\n")
-        #     sys.exit()
-
-        print(f"# Keys: {len(tree.keys())}")
-
+        print(f"# Keys: {len(list(tree.keys()))}")
 
     except KeyboardInterrupt:
         print("Terminating workers...")
